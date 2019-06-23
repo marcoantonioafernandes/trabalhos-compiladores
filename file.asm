a9cdd43b7697f058a092d4e89dda40668bca6638
Vinicius <soares7vinicius>
2019-06-23 19:27:56 -0300

    Compilando, executando e visualizando codigo objeto

--- file.asm
+++ file.asm
@@ -10,7 +10,13 @@ push dword [_@DSP + (4)]
 mov ebp, esp
 mov [_@DSP + 4], ebp
 sub esp, 4
-push dword [EBP + (16)]push dword [EBP + (12)]pop dword[ebp + (20)]add esp, 4
+push dword [EBP + (16)] 
+push dword [EBP + (12)] 
+pop eax 
+imul eax, dword [ESP] 
+mov dword [ESP], eax 
+pop dword[ebp + (20)] 
+add esp, 4
 mov esp, ebp
 pop dword [_@DSP + 4]
 pop ebp
@@ -21,10 +27,13 @@ push dword [_@DSP + (0)]
 mov ebp, esp
 mov [_@DSP + 0], ebp
 sub esp, 8
-push 4
-push 2
-call _funcaoadd esp, 8 pop dword[ebp + (-4)]mov dword[_@DSP +0 ], ebp
-push dword[ebp +-4 ]
+push 4 
+push 2 
+call _funcao
+add esp, 8 
+pop dword[ebp + (-4)] 
+mov dword[_@DSP +0 ], ebp
+push dword[ebp + (-4) ]
 push _@STR1
 call _printf
 add esp, 8
@@ -34,4 +43,5 @@ pop dword [_@DSP + 0]
 pop ebp
 ret
 section .data
-_@STR1: db '%d', 0_@DSP: times 8 db 0
+_@STR1: db '%d', 0
+_@DSP: times 8 db 0
